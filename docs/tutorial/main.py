61966019f2fe2eeae9440a093d9475af38cb3bad
shako <shako@caios.io>
2022-02-09 09:17:12 +0000

    changed the tutorial code

--- docs/tutorial/main.py
+++ docs/tutorial/main.py
@@ -1,28 +1,52 @@
 import requests
-import jdict
-import json
 
-from subprocess import run
-from os import environ, path
+import jdict
+
 
 def _upload_file() -> str:
-    cwd = path.dirname(__file__)
-    environ["PATH"] = f'{environ["PATH"]}:{cwd}'
-    cmd = 'curl -F "file=@test.txt" https://api.anonfiles.com/upload'
-    result = run(cmd, capture_output=True, env=environ, text=True, check=True, shell=True)
-    result_dict = json.loads(result.stdout)
-    return result_dict['data']['file']['metadata']['id']
+    file = {"file": open("test.txt", "rb")}
+    result = requests.post("https://api.anonfiles.com/upload", files=file)
+    result_dict = result.json()
+    # Feel the pain here
+    return result_dict["data"]["file"]["metadata"]["id"]
+
 
 def _get_file_info() -> dict:
     url = f"https://api.anonfiles.com/v2/file/{_upload_file()}/info"
     return requests.get(url).json()
 
-def _apply_jdict() -> jdict.jdict:
-    file = _get_file_info()
-    jdict.set_codec()
-    return jdict.jdict(file)
 
-data = _apply_jdict()
-print(type(data))
-print(data.data.keys())
-print(data.data.file)
+def _convert_to_jdict() -> jdict.jdict:
+    jdict.set_codec()
+    return jdict.jdict(_get_file_info())
+
+
+if __name__ == "__main__":
+    """
+    Sample data:
+    {
+        "status": True,
+        "data": {
+            "file": {
+                "url": {
+                    "short": "https://anonfiles.com/t3l5S2Gex3",
+                    "full": "https://anonfiles.com/t3l5S2Gex3/test_txt",
+                },
+                "metadata": {
+                    "size": {"bytes": 19, "readable": "19 B"},
+                    "name": "test_txt",
+                    "id": "t3l5S2Gex3",
+                },
+            }
+        },
+    }
+    """
+    data = _convert_to_jdict()
+    # Get the id:
+    print("ID: ", data.data.file.metadata.id)
+    # Use in for loop:
+    for key, value in data.data.file.metadata.items():
+        print(key, value)
+    # Assign values easily:
+    data.data.file.metadata.id = "MYID"
+    print("ID: ", data.data.file.metadata.id)
