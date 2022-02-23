7857f7b4421a68840cbed06f2180fe7910b8fd6a
Bakhtiyar_Garashov <bakhtiyar.garashov@mooncascade.com>
2022-02-23 09:02:25 +0200

    Snake case method names and docstring for function

--- src/pyjdict/transformer.py
+++ src/pyjdict/transformer.py
@@ -11,7 +11,7 @@ class JdictTransformer(ast.NodeTransformer):
     for each node found. Inherits from class NodeTransformer
     """
 
-    def visit_Module(self, node: ast.AST) -> Any:
+    def visit_module(self, node: ast.AST) -> Any:
         """
         Method imports jdict module into ast
         :param node: CodeType
@@ -24,12 +24,17 @@ class JdictTransformer(ast.NodeTransformer):
         visited_node.body.insert(0, import_node)
         return visited_node
 
-    def visit_Name(self, node: ast.Name) -> Any:
+    def visit_name(self, node: ast.Name) -> Any:
+        """
+        Method checks if the id dict and modifies it to jdict
+        :param node:
+        :return: node
+        """
         if node.id == "dict":
             node.id = JDICT
         return self.generic_visit(node)
 
-    def visit_Dict(self, node: ast.AST) -> Any:
+    def visit_dict(self, node: ast.AST) -> Any:
         """
         Method goes into the dict node and modifies it to jdict
         :param node:
